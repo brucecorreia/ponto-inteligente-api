ffe348d82924db26605e2a143bc220d3e7c34f93
Bruce <bruce_correia@hotmail.com>
2017-12-02 13:03:42 -0300

    Adicionando o Swagger ao projeto

---
+++ src/main/resources/db/migration/postgresql/V2__admin_padrao.sql
@@ -0,0 +1,8 @@
+INSERT INTO "public"."empresa" ("cnpj", "data_atualizacao", "data_criacao", "razao_social") 
+VALUES ('82198127000121', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Bruce IT');
+
+INSERT INTO "public"."funcionario" ("cpf", "data_atualizacao", "data_criacao", "email", "nome", 
+"perfil", "qtd_horas_almoco", "qtd_horas_trabalho_dia", "senha", "valor_hora", "empresa_id") 
+VALUES ('16248890935', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'admin@bruce.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL, 
+'$2a$06$xIvBeNRfS65L1N17I7JzgefzxEuLAL0Xk0wFAgIkoNqu9WD6rmp4m', NULL, 
+(SELECT id FROM empresa WHERE cnpj = '82198127000121'));
